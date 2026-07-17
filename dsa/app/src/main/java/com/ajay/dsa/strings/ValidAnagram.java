eb64ccc06880bbf636d0cfbb5c1ea9b6f4e52645
Ajay <ajay5a1990@gmail.com>
2026-07-17 12:00:45 +0530

    Fix Valid Anagram: compare character frequency counts, not string reversal

--- dsa/app/src/main/java/com/ajay/dsa/strings/ValidAnagram.java
+++ dsa/app/src/main/java/com/ajay/dsa/strings/ValidAnagram.java
@@ -1,17 +1,30 @@
 package com.ajay.dsa.strings;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class ValidAnagram {
 
     public static boolean is(String wordOne, String wordTwo){
         if(wordOne.length() != wordTwo.length()){
             return false;
         }
-        int wordLength = wordOne.length();
-        for(int i=0; i<wordOne.length(); i++){
-            if(wordOne.charAt(i) != wordTwo.charAt(wordLength-i-1)){
+        Map<Character, Integer> frequencyCountFromWordOne = new HashMap<Character, Integer>();
+        Map<Character, Integer> frequencyCountFromWordTwo = new HashMap<Character, Integer>();
+
+        for(int i = 0; i< wordOne.length(); i++){
+            frequencyCountFromWordOne.put(wordOne.charAt(i), frequencyCountFromWordOne.getOrDefault(wordOne.charAt(i), 0)+1);
+            frequencyCountFromWordTwo.put(wordTwo.charAt(i), frequencyCountFromWordTwo.getOrDefault(wordTwo.charAt(i), 0)+1);
+        }
+
+        for(int i =0; i< wordOne.length(); i++){
+            if(!Objects.equals(frequencyCountFromWordOne.getOrDefault(wordOne.charAt(i), 0), frequencyCountFromWordTwo.getOrDefault(wordOne.charAt(i), 0))){
                 return false;
             }
         }
+
         return true;
+
     }
 }
--- dsa/app/src/test/java/com/ajay/dsa/strings/ValidAnagramTest.java
+++ dsa/app/src/test/java/com/ajay/dsa/strings/ValidAnagramTest.java
@@ -7,8 +7,9 @@ public class ValidAnagramTest {
 
     @Test
     public void test(){
-
-        Assertions.assertTrue(ValidAnagram.is("abcd", "dcba"));
-        Assertions.assertFalse(ValidAnagram.is("abcd", "cba"));
+        Assertions.assertTrue(ValidAnagram.is("anagram", "nagaram"));
+        Assertions.assertTrue(ValidAnagram.is("", ""));
+        Assertions.assertFalse(ValidAnagram.is("aacc", "ccac"));
+        Assertions.assertFalse(ValidAnagram.is("Dog", "god"));
     }
 }
